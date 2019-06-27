450c7f5eb37a7994a331a092a41e00ea4e03e42d
priyanka-mehta <priyankamehta929@gmail.com>
2019-06-27 18:49:02 +0530

    Changes

---
+++ src/data.js
@@ -0,0 +1,103 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Micromax - Black",
+    img: 'img/micromax.jpg',
+    price: 10000,
+    company: "Micromax",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "I-Phone - White",
+    img: 'img/iphone.jpg',
+    price: 10,
+    company: "Apple",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Nokia - Black",
+    img: 'img/nokia.jpg',
+    price: 10,
+    company: "Nokia",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "MI A3",
+    img: 'img/mi.jpg',
+    price: 10,
+    company: "MI",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+
+  {
+    id: 5,
+    title: "Samsung",
+    img: 'img/samsung.jpg',
+    price: 10,
+    company: "Samsung",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Gionee A1",
+    img: 'img/gionee.jpg',
+    price: 10,
+    company: "Gionee",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "One Plus 7",
+    img: 'img/OnePlus-7.jpg',
+    price: 10,
+    company: "One Plus",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "LG - White",
+    img: 'img/lg.jpg',
+    price: 10,
+    company: "LG",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixal - Black",
+  img: 'img/product-1.png',
+  price: 10,
+  company: "Google",
+  info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+  inCart: false,
+  count: 0,
+  total: 0
+}
